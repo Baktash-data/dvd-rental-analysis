0388c1c32fd1e38c3dfdb22066545d563a6f3189
Baktash-data <baktashhamidi@yahoo.com>
2025-06-09 21:19:39 +0200

    Update Q10_avg_sunday_revenue.sql

--- queries/Q10_avg_sunday_revenue.sql
+++ queries/Q10_avg_sunday_revenue.sql
@@ -1,22 +1,14 @@
-SELECT AVG(total_amount_per_day),
-       day_name
-FROM
-
-(SELECT day_name,
-       total_amount_per_day
-FROM
-
-(SELECT TO_CHAR(pay_date, 'Day') AS day_name,
-       total_amount_per_day
-FROM
-
-(SELECT DATE(payment_date) AS pay_date,
-       SUM(amount) AS total_amount_per_day
-      
-FROM payment
-
-GROUP BY 1) AS amount_day) AS days_amounts
-
-WHERE day_name LIKE'%Sun%') AS sunday_amounts
-
-GROUP BY day_name
+-- Average Sunday revenue
+SELECT 
+    day_of_week,
+    ROUND(AVG(total_amount), 2) AS avg_revenue
+FROM (
+    SELECT 
+        DATE(payment_date) AS payment_day,
+        TO_CHAR(payment_date, 'Day') AS day_of_week,
+        SUM(amount) AS total_amount
+    FROM payment
+    GROUP BY payment_day, day_of_week
+) AS daily_totals
+WHERE day_of_week LIKE 'Sunday%'
+GROUP BY day_of_week;
