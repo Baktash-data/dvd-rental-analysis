5cabb937c6fe6234eb35f716f7ba0fc4d0e570a0
Baktash-data <baktashhamidi@yahoo.com>
2025-06-09 21:18:57 +0200

    Update Q9_avg_customer_spend_by_staff.sql

--- queries/Q9_avg_customer_spend_by_staff.sql
+++ queries/Q9_avg_customer_spend_by_staff.sql
@@ -1,12 +1,13 @@
-SELECT staff_id,
-       ROUND(AVG(total_per_customer),2)
-
-FROM
-(SELECT staff_id, 
-       customer_id,
-       SUM(amount) AS total_per_customer
-FROM payment
-GROUP BY 1,2
-ORDER By staff_id) AS total_customer
-
-GROUP BY staff_id
+-- Average customer spend by staff
+SELECT 
+    staff_id,
+    ROUND(AVG(total_per_customer), 2) AS avg_customer_spend
+FROM (
+    SELECT 
+        staff_id, 
+        customer_id,
+        SUM(amount) AS total_per_customer
+    FROM payment
+    GROUP BY staff_id, customer_id
+) AS customer_totals
+GROUP BY staff_id;
