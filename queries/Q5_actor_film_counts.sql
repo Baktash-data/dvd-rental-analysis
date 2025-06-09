9724677a06ebcc3296610f7629dda263ac2146e6
Baktash-data <baktashhamidi@yahoo.com>
2025-06-09 21:17:01 +0200

    Update Q5_actor_film_counts.sql

--- queries/Q5_actor_film_counts.sql
+++ queries/Q5_actor_film_counts.sql
@@ -1,9 +1,10 @@
-SELECT act.actor_id,
-       first_name,
-       last_name,
-       COUNT(film_id)
-FROM actor AS act
-LEFT JOIN film_actor AS fa
-    ON act.actor_id = fa.actor_id
-GROUP BY act.actor_id, first_name, last_name
-ORDER BY count DESC
+-- Actor film appearances
+SELECT 
+    a.actor_id,
+    a.first_name,
+    a.last_name,
+    COUNT(fa.film_id) AS film_count
+FROM actor a
+LEFT JOIN film_actor fa ON a.actor_id = fa.actor_id
+GROUP BY a.actor_id, a.first_name, a.last_name
+ORDER BY film_count DESC;
