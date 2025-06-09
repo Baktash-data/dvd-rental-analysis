fe9aa5486520afe686c56ce005ae1b5f6f1d2591
Baktash-data <baktashhamidi@yahoo.com>
2025-06-09 21:16:40 +0200

    Update Q4_film_count_by_category.sql

--- queries/Q4_film_count_by_category.sql
+++ queries/Q4_film_count_by_category.sql
@@ -1,9 +1,9 @@
-SELECT COUNT(title),
-       name
-FROM film AS f
-LEFT JOIN film_category AS fc
-    ON f.film_id = fc.film_id
-LEFT JOIN category AS cat
-    ON fc.category_id = cat.category_id
-GROUP BY name
-ORDER BY COUNT(title) DESC
+-- Film count per category
+SELECT 
+    c.name AS category,
+    COUNT(f.film_id) AS film_count
+FROM film f
+LEFT JOIN film_category fc ON f.film_id = fc.film_id
+LEFT JOIN category c ON fc.category_id = c.category_id
+GROUP BY c.name
+ORDER BY film_count DESC;
