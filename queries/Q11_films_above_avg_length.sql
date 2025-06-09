f01396401dffabb813d88e6e079b443968d4f6b2
Baktash-data <baktashhamidi@yahoo.com>
2025-06-09 21:20:01 +0200

    Update Q11_films_above_avg_length.sql

--- queries/Q11_films_above_avg_length.sql
+++ queries/Q11_films_above_avg_length.sql
@@ -1,8 +1,13 @@
-SELECT film_id, 
-       title,
-       length,
-       replacement_cost
-FROM film AS f1
-WHERE length > (SELECT AVG(length) FROM film AS f2 WHERE f1.replacement_cost = f2.replacement_cost )
-
-ORDER BY length
+-- Films longer than category average
+SELECT 
+    f.film_id,
+    f.title,
+    f.length,
+    f.replacement_cost
+FROM film f
+WHERE f.length > (
+    SELECT AVG(length)
+    FROM film 
+    WHERE replacement_cost = f.replacement_cost
+)
+ORDER BY f.length;
