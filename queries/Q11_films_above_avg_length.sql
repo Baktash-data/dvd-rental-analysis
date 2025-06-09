4b959222877a7089b8edad544432e336a96aa79e
Baktash-data <baktashhamidi@yahoo.com>
2025-06-09 21:14:22 +0200

    Create Q11_films_above_avg_length.sql

---
+++ queries/Q11_films_above_avg_length.sql
@@ -0,0 +1,8 @@
+SELECT film_id, 
+       title,
+       length,
+       replacement_cost
+FROM film AS f1
+WHERE length > (SELECT AVG(length) FROM film AS f2 WHERE f1.replacement_cost = f2.replacement_cost )
+
+ORDER BY length
